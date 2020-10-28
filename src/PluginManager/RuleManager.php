aefe17a1d83202d98edde0a29d725928b4070401
Andrew Riddlestone <andrew.riddlestone@gmail.com>
2020-10-28 18:51:56 +0000

    fix(acl): More specific parameter types for rule providers

--- src/PluginManager/RuleManager.php
+++ src/PluginManager/RuleManager.php
@@ -2,6 +2,8 @@
 
 namespace Riddlestone\Brokkr\Acl\PluginManager;
 
+use Laminas\Permissions\Acl\Role\RoleInterface;
+use Riddlestone\Brokkr\Acl\ResourceInterface;
 use Riddlestone\Brokkr\Acl\RuleInterface;
 
 class RuleManager extends AbstractProviderManager implements RuleManagerInterface
@@ -14,8 +16,8 @@ class RuleManager extends AbstractProviderManager implements RuleManagerInterfac
     protected $instanceOf = RuleProviderInterface::class;
 
     /**
-     * @param array $roles
-     * @param array $resources
+     * @param RoleInterface[] $roles
+     * @param ResourceInterface[] $resources
      * @return RuleInterface[]
      */
     public function getRules(array $roles, array $resources)
--- src/PluginManager/RuleProviderInterface.php
+++ src/PluginManager/RuleProviderInterface.php
@@ -2,13 +2,15 @@
 
 namespace Riddlestone\Brokkr\Acl\PluginManager;
 
+use Laminas\Permissions\Acl\Role\RoleInterface;
+use Riddlestone\Brokkr\Acl\ResourceInterface;
 use Riddlestone\Brokkr\Acl\RuleInterface;
 
 interface RuleProviderInterface
 {
     /**
-     * @param array $roles
-     * @param array $resources
+     * @param RoleInterface[] $roles
+     * @param ResourceInterface[] $resources
      * @return RuleInterface[]
      */
     public function getRules(array $roles, array $resources);
